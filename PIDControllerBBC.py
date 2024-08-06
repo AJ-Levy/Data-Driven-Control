45fbd2d4ca097326c176d436ad67634d8f84cf9e
Ariel J. Levy <ariellevy54@gmail.com>
2024-08-06 21:25:31 +0200

    PID control on buck converter now works

--- PIDControllerBBC.py
+++ PIDControllerBBC.py
@@ -3,16 +3,31 @@ class PIDController:
     Class to store values for the PID Controller,
     and to determine output signal. 
     '''
-    def __init__(self, Kp, Ki, Kd, dt=None):
+    def __init__(self, Kp, Ki, Kd, dt=None, saturation_min = 0.1, saturation_max = 0.9):
         self.Kp = Kp
         self.Ki = Ki
         self.Kd = Kd
         self.dt = dt
-        self.integral_error = 0
+        self.integral_error = 0.1
         self.previous_error = 0
         self.previous_time = None
-        self.dt = None
-        self.scale_factor = 1/350000
+        self.saturation_min = saturation_min
+        self.saturation_max = saturation_max
+        self.in_saturation = False
+
+    def saturation(self, out, saturation_min, saturation_max):
+        '''
+        Ensures the output remains between the saturation limits.
+        '''
+        if out > saturation_max:
+            self.in_saturation = True
+            return saturation_max
+        elif out < saturation_min:
+            self.in_saturation = True
+            return saturation_min
+        else:
+            self.in_saturation = False
+        return out
 
     def update(self, voltage, time):
         '''
@@ -23,14 +38,16 @@ class PIDController:
 
         if self.previous_time is None:
             self.previous_time = time
-            # return intial duty cycle of 0.5
-            return 0.5
+            # return intial duty cycle
+            intial_output = self.Kp * error + self.Ki * self.integral_error
+            return self.saturation(intial_output, self.saturation_min, self.saturation_max)
 
         # Calculate the time difference
         self.dt = time - self.previous_time
 
-        # Integral error
-        self.integral_error += error * self.dt
+        # integral error (anti-windup measures included)
+        if not (self.in_saturation):
+            self.integral_error += error * self.dt
 
         # Derivative error
         derivative_error = (error - self.previous_error) / self.dt
@@ -44,14 +61,17 @@ class PIDController:
         self.previous_error = error
         self.previous_time = time
 
-        return output * self.scale_factor
+        saturated_output = self.saturation(output, self.saturation_min, self.saturation_max)
+        
+        return saturated_output
 
 # Create PID object
-pid_controller = PIDController(Kp=100, Ki=10, Kd=1)
+pid_controller = PIDController(Kp=0.01, Ki=5, Kd=0)
 
 def controller_call(voltage, time):
     '''
     Method that MATLAB calls for PID.
     '''
+    global pid_controller
     signal = pid_controller.update(voltage, time)
     return signal
--- PIDMainBBC.py
+++ PIDMainBBC.py
@@ -2,12 +2,11 @@ import matlab.engine
 import matplotlib.pyplot as plt
 import numpy as np
 
-def main(desired_voltage = 12,
-         source_voltage = 50,
-         model = 'bbcSimPID',
-         mask = 'BBC',
-         block = 'V_source_value',
-         stabilisation_precision = 0.05):
+def main(desired_voltage = 30,
+         source_voltage = 48,
+         model = 'pibuckconverter',
+         block = 'input_voltage',
+         stabilisation_precision = 0.5):
     '''
     Main method to set up MATLAB, simulink,
     and handle data aquisition/plotting.
@@ -17,10 +16,10 @@ def main(desired_voltage = 12,
     eng.load_system(model, nargout=0)
 
     # Set desired output voltage
-    #eng.set_param(f'{model}/finalVoltage', 'Value', str(desired_voltage), nargout=0)
+    eng.set_param(f'{model}/finalVoltage', 'Value', str(desired_voltage), nargout=0)
     
     # Set initial voltage
-    #eng.set_param(f'{model}/{mask}/{block}', 'Amplitude', str(source_voltage), nargout=0)
+    eng.set_param(f'{model}/{block}', 'Amplitude', str(source_voltage), nargout=0)
 
     print("Running simulation...")
     eng.eval(f"out = sim('{model}');", nargout=0)
@@ -28,30 +27,34 @@ def main(desired_voltage = 12,
 
     # Get force data
     # Get angles
-    pulse_2d = eng.eval("out.pulse")
+    pulse_2d = eng.eval("pulse")
     pulse_lst = []
     for p in pulse_2d:
         pulse_lst.append(p[0])
-    print(min(pulse_lst), max(pulse_lst))
+    print(f"Average Duty Cycle: {np.median(pulse_lst):.1f}")
 
     # Get angles
-    voltage_2d = eng.eval("out.Vout")
+    voltage_2d = eng.eval("Vout")
     voltage_lst = []
     for v in voltage_2d:
         voltage_lst.append(v[0])
 
     # Get time
-    time_2d = eng.eval("out.time")
+    time_2d = eng.eval("time")
     time_lst = []
     for time in time_2d:
         time_lst.append(time[0])
 
     # Plot data
-    plt.plot(time_lst, voltage_lst, label = 'Output Signal')
+    plt.plot(time_lst, voltage_lst, label = 'Output Signal', color = "red")
+    plt.axhline(y=stabilisation_precision + desired_voltage, color='k', linestyle='--', label=f'{stabilisation_precision + desired_voltage} V')
+    plt.axhline(y=-stabilisation_precision + desired_voltage, color='k', linestyle='--', label=f'{-stabilisation_precision + desired_voltage} V')
     plt.xlabel("Time (s)")
     plt.ylabel("Voltage (V)")
     plt.title("Voltage Progression over Time")
     plt.xlim(0,max(time_lst))
+    view_range = 0.2*desired_voltage
+    plt.ylim(desired_voltage - view_range,desired_voltage + view_range)
     plt.legend()
     plt.show()
 
