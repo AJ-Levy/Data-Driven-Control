608c1116a3204a03d9861627b9d977761df7d40a
Ariel J. Levy <ariellevy54@gmail.com>
2024-08-21 22:31:17 +0200

    for v2 BBC model

--- PIDMainBBC.py
+++ PIDMainBBC.py
@@ -14,9 +14,9 @@ def setNoise(eng, model, noise):
         eng.set_param(f'{model}/Noise', 'Cov', str([0]), nargout=0)
 
 def main(noise = False,
-        desired_voltage = 30,
+        desired_voltage = -110,
          source_voltage = 48,
-         model = 'bbcSimPID',
+         model = 'bbcSimPIDv2',
          block = 'BBC',
          stabilisation_precision = 0.5):
     '''
@@ -27,7 +27,7 @@ def main(noise = False,
     eng = matlab.engine.start_matlab()
     eng.load_system(model, nargout=0)
 
-    setNoise(eng, model, noise)
+    #setNoise(eng, model, noise)
     eng.set_param(f'{model}/finalVoltage', 'Value', str(desired_voltage), nargout=0)
         
     # Set initial voltage
@@ -46,10 +46,10 @@ def main(noise = False,
     print(f"Average Duty Cycle: {np.mean(pulse_lst):.3f}")
 
     # Get voltages
-    voltage_2d = eng.eval("out.Vout")
+    voltage_2d = eng.eval("out.voltage")
     voltage_lst = []
     for v in voltage_2d:
-        voltage_lst.append(-v[0])
+        voltage_lst.append(v[0])
 
     # Get time
     time_2d = eng.eval("out.time")
@@ -59,8 +59,8 @@ def main(noise = False,
 
     # Plot data
     plt.plot(time_lst, voltage_lst, label = f'{desired_voltage} V')
-    plt.axhline(y=stabilisation_precision + (-desired_voltage), color='k', linestyle='--', label=f'{stabilisation_precision + (-desired_voltage)} V')
-    plt.axhline(y=-stabilisation_precision + (-desired_voltage), color='k', linestyle='--', label=f'{-stabilisation_precision + (-desired_voltage)} V')
+    plt.axhline(y=stabilisation_precision + (desired_voltage), color='k', linestyle='--', label=f'{stabilisation_precision + (desired_voltage)} V')
+    plt.axhline(y=-stabilisation_precision + (desired_voltage), color='k', linestyle='--', label=f'{-stabilisation_precision + (desired_voltage)} V')
     plt.xlabel("Time (s)")
     plt.ylabel("Voltage (V)")
     #plt.title("Voltage Progression over Time")
@@ -73,4 +73,4 @@ def main(noise = False,
     eng.quit()
 
 if __name__ == '__main__':
-    main(noise=True)
+    main(noise=False)
