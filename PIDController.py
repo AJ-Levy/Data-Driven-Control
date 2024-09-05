bc63cbe0fba937cb82ffacb96472723d9f0be8ac
Ariel J. Levy <ariellevy54@gmail.com>
2024-09-05 20:59:10 +0200

    improve commenting of PIDController rest will be done like this

--- PIDController.py
+++ PIDController.py
@@ -1,9 +1,26 @@
 class PIDController:
     '''
-    Class to store values for the PID Controller,
-    and to determine output force. 
+    A class that implements simple a Proportional-Integral-Derivative (PID) controller.
+    
+    Attributes:
+        Kp (float): Proportional gain coefficient.
+        Ki (float): Integral gain coefficient.
+        Kd (float): Derivative gain coefficient.
+        dt (float): Step size.
+        integral_error (float): Accumulated integral of the error.
+        previous_error (float): Error value from the previous update.
     '''
+
     def __init__(self, Kp, Ki, Kd, dt):
+        '''
+        Initialises the PID controller with the specified gain coefficients and step siz.
+
+        Args:
+            Kp (float): Proportional gain.
+            Ki (float): Integral gain.
+            Kd (float): Derivative gain.
+            dt (float): Step size.
+        '''
         self.Kp = Kp
         self.Ki = Ki
         self.Kd = Kd
@@ -13,33 +30,43 @@ class PIDController:
 
     def update(self, setpoint, measurement):
         '''
-        Determines the output force accoring to PID.
+        Computes the control output using the PID control equation.
+
+        Args:
+            setpoint (float): Desired value.
+            measurement (float): Current value.
+
+        Returns:
+            float: Output force.
         '''
-        # Proportional error
+
         error = measurement - setpoint
 
-        # Integral error
         self.integral_error += error * self.dt
 
-        # Derivative error
         derivative_error = (error - self.previous_error) / self.dt
 
-        # Calculate the output force
         output_force = (self.Kp * error +
                         self.Ki * self.integral_error +
                         self.Kd * derivative_error)
 
-        # Update variables
         self.previous_error = error
 
         return output_force
 
-# Create PID object
+# Instantiate PIDController object.
 pid_controller = PIDController(Kp=44, Ki=80, Kd=6, dt=0.001)
 
 def controller_call(setpoint, measurement):
     '''
-    Method that MATLAB calls for PID.
+    Calls the PID controller to compute the control signal.
+
+    Args:
+        setpoint (float): Desired value.
+        measurement (float): Current value.
+
+    Returns:
+        float: Output force.
     '''
     force = pid_controller.update(setpoint, measurement)
     return force
